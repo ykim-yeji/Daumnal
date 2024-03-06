3838bf4a7a7a84526406b2fc7bb39c2d69a14548
김예지 <ilice0403@naver.com>
2024-03-06 17:44:22 +0900

    chore: 프로젝트 생성
    Former-commit-id: 0d67dfc7a8368cc9d6e569245e37b74b7e88f3a3

---
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/DaumnalApplication.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4aa6e59fe18207166a4296b8a41344b2a71e8bb1ab51202312b492c6ba0d8ed0
+size 324
---
+++ backend/daumnal/src/test/java/com/ssafy/daumnal/DaumnalApplicationTests.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:480bc79d0674aefa165b1bbf791b75bf6eba8d31162d1fd4696a0b03443437d0
+size 219
