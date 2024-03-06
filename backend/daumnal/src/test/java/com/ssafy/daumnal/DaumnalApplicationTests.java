8fa3b2e07617bcc4ffad7379cceb448e717be104
김예지 <ilice0403@naver.com>
2024-03-06 17:44:22 +0900

    chore: 프로젝트 생성
    Former-commit-id: 0d67dfc7a8368cc9d6e569245e37b74b7e88f3a3

---
+++ backend/daumnal/src/test/java/com/ssafy/daumnal/DaumnalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.daumnal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaumnalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
