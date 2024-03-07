fac0a45d305f9eff951bb279c6145654991719a4
김예지 <ilice0403@naver.com>
2024-03-07 16:42:40 +0900

    feat: BackgroundMusic 엔티티 id 외 컬럼 작성
    Former-commit-id: 1079ce5df3d4757e9bb1f6c29d06d105a5c51dcb

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/BackgroundMusic.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/BackgroundMusic.java
@@ -1,16 +1,3 @@
-package com.ssafy.daumnal.music.entity;
-
-import com.ssafy.daumnal.global.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.Getter;
-
-@Entity
-@Getter
-@Table(name = "background-music")
-public class BackgroundMusic extends BaseEntity {
-
-    @Id
-    @Column(name = "id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:74d328fa20d9ba91bd9dd40be48a85ef5400d5614872905ee114ec26a0916aa4
+size 642
