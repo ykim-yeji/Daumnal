06d483552a21da6036279341833b616cbbcaee4d
김예지 <ilice0403@naver.com>
2024-03-07 16:37:03 +0900

    feat: Diary 엔티티 생성
    Former-commit-id: 76a0dddf187455ec77d9a2c821838b2bb9b10fcb

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/entity/Diary.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/entity/Diary.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1e7409110ac88eacf0e2b6af70b13224893958a38ebbe5e1e6acb14acb7f4f0
-size 705
+oid sha256:89c32ebe261af6caa695d74aeef595d98fb200939e1ad57333837790f38a59fc
+size 1395
