76a0e9b6332cf7609d9c86bfa6e2f76dd686256f
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-03-07 15:29:05 +0900

    test: jieun2
    Former-commit-id: 53b09f775107abcbeea8ee42418464a8beaf6618

--- frontend/daumnal/src/App.tsx
+++ frontend/daumnal/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93831d2e2bf861985a130efd14cb79805e6465325f4daba828ebae502cc21113
-size 349
+oid sha256:8cc9ea5124a88e59a0aa70fdd04bd87359943dfd05083a863b663f49fb4dc572
+size 350
