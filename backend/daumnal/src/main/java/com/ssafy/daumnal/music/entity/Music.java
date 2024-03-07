c1260be563a5e4a27515a239c7144b2634a5d017
김예지 <ilice0403@naver.com>
2024-03-07 17:00:15 +0900

    feat: Music 엔티티 id 외 컬럼 작성
    Former-commit-id: c5997982e41aacc63d839431131cb88a659ea1fb

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/Music.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/Music.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87891fa3082a50c20180848e3f302376bcb0ed2f86b777d4cae7d859e49998e6
-size 354
+oid sha256:c2d15a791f6ec4021abe201cff839226ce81d85f8cae70ea66becc48297fe5d7
+size 1280
