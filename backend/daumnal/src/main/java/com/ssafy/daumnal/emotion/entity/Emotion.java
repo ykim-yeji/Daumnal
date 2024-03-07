58fbd0f84b5fd60f6776b2b6f4b7303ad8197fe9
김예지 <ilice0403@naver.com>
2024-03-07 16:39:15 +0900

    feat: emotion 엔티티 id 외 컬럼 작성
    Former-commit-id: bbca3c3305a4bb4137dc99b89704dfab6f43292c

--- backend/daumnal/src/main/java/com/ssafy/daumnal/emotion/entity/Emotion.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/emotion/entity/Emotion.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d707ac49e59c5c45f8cbd02ad0fa3bbd41d94be1496b5a75610b5b215ffc2daf
-size 360
+oid sha256:1dd3493e4ab22f4f05abf47702f92b9ddc62f46bed4366b852d707344038889e
+size 748
