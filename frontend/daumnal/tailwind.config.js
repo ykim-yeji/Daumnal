cf82310a912299163f542ed6f7109a34c81d9c65
jojoo <csh990720@gmail.com>
2024-03-07 17:23:58 +0900

    chore: .tsx 파일에 export 추가/ tailwind 색상 코드 추가
    Former-commit-id: 91654a7dfdb332465e5b38781dc590b8237e0323

--- frontend/daumnal/tailwind.config.js
+++ frontend/daumnal/tailwind.config.js
@@ -1,11 +1,3 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:513afb23e648b65362de907371513ee0cf31eede46afa6706dd0c47233f56134
+size 516
