f5615961bc648953bf74609787932585a59b7b77
김예지 <ilice0403@naver.com>
2024-03-07 17:40:19 +0900

    feat: S3ServiceImpl getFileUuidName, uuid 이용해 파일 이름 만드는 메소드 생성
    Former-commit-id: 4c8ed2230a60829b88e28dc0859eaed46d11b07d

--- backend/daumnal/src/main/java/com/ssafy/daumnal/s3/service/impl/S3ServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/s3/service/impl/S3ServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff88a4c816a79c51f584ca33b967e24cdf6c59ff4e2367ac92c511c1370edf2f
-size 3820
+oid sha256:f49471a315b036dcda514aba28aa3bb6bf789a54a4083b04025660ecbc91cfaa
+size 4162
