ca85d25e35dbcdc1dee270ea1a8a5e78fed25479
김예지 <ilice0403@naver.com>
2024-03-07 16:39:15 +0900

    feat: emotion 엔티티 id 외 컬럼 작성
    Former-commit-id: bbca3c3305a4bb4137dc99b89704dfab6f43292c

--- backend/daumnal/src/main/java/com/ssafy/daumnal/emotion/entity/Emotion.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/emotion/entity/Emotion.java
@@ -1,16 +1,3 @@
-package com.ssafy.daumnal.emotion.entity;
-
-import com.ssafy.daumnal.global.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.Getter;
-
-@Entity
-@Getter
-@Table(name = "emotion")
-public class Emotion extends BaseEntity {
-
-    @Id
-    @Column(name = "id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:1dd3493e4ab22f4f05abf47702f92b9ddc62f46bed4366b852d707344038889e
+size 748
