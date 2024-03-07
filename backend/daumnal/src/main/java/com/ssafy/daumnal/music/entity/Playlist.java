d761c7603681eda4deaf1a017b6c205d8a1633b2
김예지 <ilice0403@naver.com>
2024-03-07 17:27:30 +0900

    feat: Playlist 엔티티 id 외 컬럼 작성
    Former-commit-id: 60fc9881e635fb5fed45a0d406254730473b9373

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/Playlist.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/Playlist.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6adb9da2f9d21309d559cce578726ea5fbc7f583bec22a71978a3d0d6f348792
-size 360
+oid sha256:0c21502bc7564ab9f2809a9e7ee4cf14d13de6bede7988add56fab39e092fad1
+size 930
