229dc9dc6c3dcfe9ab975c03d7473fde410d891a
core <jinjs6@gmail.com>
2024-03-11 15:06:35 +0900

    feat: 예외 처리 문구 생성
    api 명세서에 작성한 예외처리 문구를 코드에 반영하였습니다.
    
    
    Former-commit-id: 10833bf08de4c37ea4067bc4d201e2be9b1340f8

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/ErrorCode.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/ErrorCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d7d5cd896c0fd3518401a1134bc13f8b0812b26c0f879931e9bc6910e025a68
-size 603
+oid sha256:3b0e9f6a66c8eaf340a3bd4dcd618706176056cd4ab39629049ae5d9ab699d82
+size 3819
