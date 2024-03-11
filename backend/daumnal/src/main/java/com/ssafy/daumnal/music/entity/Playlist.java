6e3400e1e8b6aea9542ac8234d4a491ac892b78a
김예지 <ilice0403@naver.com>
2024-03-11 16:52:29 +0900

    fix: Music, Playlist Entity 에러 수정
    - @ColumnDefault 안의 값 '' 작은따옴표로 문자열 형식 부여
    
    
    Former-commit-id: 6adf26bdea6f375b1376b04d9501e1dd0c044fd3

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/Playlist.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/Playlist.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b03f2e3d6aa26ea9d1ff6713c3751166bd23859ef3307ae19fa52d12e07d03b8
-size 1001
+oid sha256:7d9b106836896ffae8d6a654e44391ebd08b17bc3cec9c1d9a4a762ea532ed40
+size 1003
