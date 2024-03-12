447eb391aa321b7444f8c80e553e80055d6d2d2c
core <jinjs6@gmail.com>
2024-03-12 10:20:14 +0900

    feat: 회원 가입 예외처리 로직 구현
    예외 처리의 내용은 다음과 같습니다.
    - socialId 입력 여부 확인
    - socialProvider 입력 여부 확인
    - socialId 입력 형태  확인
    - socialProvider 입력 정보가 kakao인지 naver인지 확인
    - 이미 존재한 회원인지 확인
    
    
    Former-commit-id: a5980820130e04ebba330d9978fe242c165f01d6

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/repository/MemberRepository.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/repository/MemberRepository.java
@@ -1,9 +1,3 @@
-package com.ssafy.daumnal.member.repository;
-
-import com.ssafy.daumnal.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:376e927ebdfd05989b66312f241f8f25da40d5f7ae6460924bf61a7a024407c2
+size 446
