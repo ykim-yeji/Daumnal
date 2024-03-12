3fe8ed14afb21df65ce6fa14391a66b5d2008178
core <jinjs6@gmail.com>
2024-03-12 12:36:44 +0900

    feat: 회원 닉네임 입력 관련 예외처리 진행
    예외처리 사항은 다음과 같습니다.
    - memberId 자연수 입력 여부
    - 로그아웃 여부
    - 회원 탈퇴 여부
    - 회원 존재 여부
    - 닉네임 입력 여부
    - 닉네임 한글 또는 영어 입력 여부
    - 닉네임 길이 15자 이하 제한
    - 이미 존재한 닉네임을 입력 제한
    
    
    Former-commit-id: f4e90f7e7ec30c391812cdfb496bec4ad78401f8

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/repository/MemberRepository.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/repository/MemberRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:376e927ebdfd05989b66312f241f8f25da40d5f7ae6460924bf61a7a024407c2
-size 446
+oid sha256:a19360ba319448f9ed5dd8a2b478d945b863f8731315850ee7e82f9e65f8a342
+size 506
--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/service/impl/MemberServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/service/impl/MemberServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ce542ad4bf68797f0c6e9f3fae3c311833eccb0a4c91ddccd00554e236bee42
-size 3122
+oid sha256:e45f56e683eae823bdd903b963da5f001102989f2402dc346f34aa4783aa4f3d
+size 4860
