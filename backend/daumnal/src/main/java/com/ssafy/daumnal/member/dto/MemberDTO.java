519de6394c4380a6447ec63f6b34a4bc5b0c0c27
core <jinjs6@gmail.com>
2024-03-12 10:44:12 +0900

    feat: 회원 닉네임 입력 관련 request dto 생성
    Former-commit-id: 33255b5b1b6f03f5ae48a063a10815eda7a82549

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/dto/MemberDTO.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/dto/MemberDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d4c4dcdb0d18b40b40b5f409f50cbe241f745839d3bbd1b0fc65c2f5221c24d
-size 223
+oid sha256:43b4fcd68635775c47bbb9309e05cf7d8cc558aa8aa068fcf0dd2c504022123a
+size 332
