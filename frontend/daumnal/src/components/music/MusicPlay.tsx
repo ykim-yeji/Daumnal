a3e46809968442163ec8b8a356997f6f4fb10198
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-03-12 14:31:07 +0900

    feat: 음악 재생 컴포넌트 기본 설정
    Former-commit-id: ff7216f11e7131ce0c56e7731a0575196a1012f4

--- frontend/daumnal/src/components/music/MusicPlay.tsx
+++ frontend/daumnal/src/components/music/MusicPlay.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a1cf8bd7f788c662f43b45db997190c48b1176e59ec0e93ef58f9f73528636b
-size 914
+oid sha256:af5760e994c9eee7b45073878b5b7a0f1bf2f6a14d89ee078d3e6f9cad16d960
+size 1648
