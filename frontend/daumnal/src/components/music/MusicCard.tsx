13be3218f3a5cb049603f061f5ffba8490f14c26
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-03-13 17:25:00 +0900

    feat: 플레이리스트 상세 컴포넌트 구현, 플레이리스트 페이지 기본 설정
    Former-commit-id: 8c1a205b336a5be18583cb3b256d91194c9cec3a

---
+++ frontend/daumnal/src/components/music/MusicCard.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ab257666d751a5ceb56090179e0fe1215656a516076917efa3219bfc6bcd9085
+size 2254
--- frontend/daumnal/src/components/music/PlaylistList.tsx
+++ frontend/daumnal/src/components/music/PlaylistList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffa41efb6c1be029c4396e6fa091f98dad2d004da446b69579147ed8e9bf6511
-size 2121
+oid sha256:a48a933e668045697a44f16aaaedca5d9387ff2a7b8878cf9c7d26991bb0bc8a
+size 2266
