70d1c8414d035c337e7d94229c05a94078c2eff8
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-03-13 17:32:09 +0900

    fix: 플레이리스트 상세 컴포넌트 재수정
    Former-commit-id: 511f970313fa7b196d1eefcb1f558b29bd5c198d

--- frontend/daumnal/src/components/music/MusicCard.tsx
+++ frontend/daumnal/src/components/music/MusicCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab257666d751a5ceb56090179e0fe1215656a516076917efa3219bfc6bcd9085
-size 2254
+oid sha256:e681dbe06e18b8de04f3cda62a7e3517291e0302ddc27b058cc1e6d1d1f0f59f
+size 2240
