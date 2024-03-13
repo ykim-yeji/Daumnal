5dbdcb045e0e0f862050b029ab0983f77db437c1
core <jinjs6@gmail.com>
2024-03-13 15:30:56 +0900

    refactor: 테스트 코드 명칭 변경
    Former-commit-id: e0fbba1500a857ed69d4833d309d4faec1b91744

--- backend/daumnal/src/test/java/com/ssafy/daumnal/global/util/JwtProviderTest.java
+++ backend/daumnal/src/test/java/com/ssafy/daumnal/global/util/JwtProviderTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0dfe77dc8618f360f1f5d783d79776f2fcd65389306ecbb6b356549a8331f239
-size 2096
+oid sha256:cbd895da7e61a627cb3708155306286cb56f8845ecd14b34c18fe8544c0a6e94
+size 2245
