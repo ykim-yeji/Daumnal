9c30c66e08e957ce81bfcac910667f2e090076a6
core <jinjs6@gmail.com>
2024-03-13 16:31:42 +0900

    feat: jwt 토큰을 통한 데이터 추출 로직 구현 및 generateToken 메서드에 회원 닉네임 정보 추가
    Former-commit-id: 43565a9ba174cea67c72b4d8d88374adbd901c2b

---
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/dto/TokenMemberDTO.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a89c3a91616fcc3af03ba564b3cd5a7d0e17a6a8c7277a0147562538165d2dd7
+size 654
--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/service/impl/MemberServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/service/impl/MemberServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e16ed019b9ead0324c87f24b46e7e1fdf2499024dc0ee746f305aa0a5237b118
-size 8448
+oid sha256:03b0e2e7fd0b6e250e4e4d74a74766ba09f679166977e43a12244f32ab8024a8
+size 8508
