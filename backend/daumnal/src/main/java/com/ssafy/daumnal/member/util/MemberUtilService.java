aef432e76c291f0c3bbf9115a7078a6859e161ae
core <jinjs6@gmail.com>
2024-03-14 14:33:50 +0900

    refactor: getProvider 메서드의 위치를  MemberUtilService 파일 위치로 변경
    Former-commit-id: 20b2335e80ead2055f0e4716ea737cf3fa3443b4

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/util/MemberUtilService.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/util/MemberUtilService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f3627992a7dff5d5bb6bf8b160b58ed57eeb72a473122cc24acef8982ba4dc1
-size 3246
+oid sha256:ada26ebd2de6193959163f2e983fa716012e9c7648041247c3d5d63dd7383bac
+size 3835
