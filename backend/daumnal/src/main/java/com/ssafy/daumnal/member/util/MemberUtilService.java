7881ec79787e8b23f11cdd95f4d30b32b2db4adf
core <jinjs6@gmail.com>
2024-03-14 13:42:25 +0900

    chore: 예외 처리 메서드에 대한 주석 스타일 변경
    Former-commit-id: dd7641c8760fb138f4d8a3ac6b381deb4598a1d0

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/util/MemberUtilService.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/util/MemberUtilService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74f8e2aecb605c6fd633908ca6e3f0a34623b9cb5f5bcff3ed9b3a6ba4dcb6a1
-size 1409
+oid sha256:ba8701a986d515403f8c3967b552e95568342e9e22ba1084f4258588453914f9
+size 1577
