69a5e9edf96888d1594b4d62c26a9adee0cb4651
core <jinjs6@gmail.com>
2024-03-14 14:56:32 +0900

    refactor: CrossOrigin 어노테이션 제거
    SecurityConfig 파일에서 이미 Cors 검증 제거 설정을 진행했기 때문에
    컨트롤러에서 어노테이션을 정의할 필요가 없습니다.
    
    
    Former-commit-id: bf0270092cb463e09107a050804f7029b69deae8

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/controller/BackgroundMusicController.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/controller/BackgroundMusicController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d149c26b7e5770b13b3b05f2bf7873787c93c11c32d7bf6724d2000a20568b94
-size 539
+oid sha256:422f216ff8a0eee4e96317b79f7c8533c93eaadca8e03358d47d337b35dd9ae8
+size 521
