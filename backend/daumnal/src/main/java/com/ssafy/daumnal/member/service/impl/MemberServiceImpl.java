3612cdf63f53f244dec4cfc407702b79af076bc1
core <jinjs6@gmail.com>
2024-03-14 13:38:03 +0900

    refactor: MemberServiceImpl의 addMember의 예외처리 관련 코드 리팩토링
    util 폴더의 MemberUtilService 파일에 addMember 메서드의 예외 처리 코드
    부분을 모아두었습니다. memberRepository를 통한 검증은 따로 모아두진
    않았습니다. 이유는 스프링 빈 의존관계 주입 단계에서 의존 관계가 꼬이는
    것을 방지하기 위함입니다.
    
    
    Former-commit-id: 37b16794323b3e66f249597a6682c5a634c817c3

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/service/impl/MemberServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/service/impl/MemberServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79a53a41c6b718ca72fed8d77069982233051e5eed393b7350083d62d9d63b0d
-size 8136
+oid sha256:d0070ffb4759906f172b5132ee38d4e1ca9e80e1c1f64360aa2d12985ed9b0f8
+size 7984
---
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/util/MemberUtilService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:74f8e2aecb605c6fd633908ca6e3f0a34623b9cb5f5bcff3ed9b3a6ba4dcb6a1
+size 1409
