298b41c674d8a424573756ad2b12da32b579c582
core <jinjs6@gmail.com>
2024-03-14 14:08:27 +0900

    refactor: addMemberNickname 예외처리 관련 코드 리팩토링 진행
    Former-commit-id: c122480128a0ec4182dc0ef00d2a134ae65a22d5

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/service/impl/MemberServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/service/impl/MemberServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ac48bc2c0e8556a7635fd003c4b020062ee86f16acdd2cc4e8792c966352df8
-size 7721
+oid sha256:33fd28847f0917599381973bc2c0cce4b61bd9864bc83e463d68583a66fb293d
+size 6763
