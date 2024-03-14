d070fcd095ceeb2f7ea9b807a2639a010355c700
core <jinjs6@gmail.com>
2024-03-14 14:27:36 +0900

    refactor: updateMemberStatusLogin 예외 처리 부분 리팩토링
    추가로 MemberUtilService 부분에서 로그인 상태 관련 예외 코드들은 확장성
    및 유지보수성을 위해서 각각 분리하였습니다.
    
    
    Former-commit-id: 61488bc6b2c35d957ed293f6cc65f67ed6094674

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/constants/MemberConstants.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/constants/MemberConstants.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88f7833fcf2b41d8e0e390d4442e2c2d4946880cf1ccb714662d8ebb08c0d136
-size 352
+oid sha256:91e589c02f2c87697b390c176bff58aa2c504dbdd469114120f61c4046ac2b7a
+size 378
