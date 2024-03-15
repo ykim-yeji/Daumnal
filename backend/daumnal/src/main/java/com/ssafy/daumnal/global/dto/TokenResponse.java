d701d15006b2cea1f538aeac7f18bbb08219de62
core <jinjs6@gmail.com>
2024-03-15 16:20:48 +0900

    feat: refreshToken 정보 추가
    Former-commit-id: e7e2d39ba1df5e830276b19c80656941101b6cbb

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/dto/TokenResponse.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/dto/TokenResponse.java
@@ -1,13 +1,3 @@
-package com.ssafy.daumnal.global.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-@AllArgsConstructor
-public class TokenResponse {
-
-    private String accessToken;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:0f22cb6ae2b96bdca8ee3a57bdee599d2106fbe789366b9d541fc5cbb1005a8b
+size 253
