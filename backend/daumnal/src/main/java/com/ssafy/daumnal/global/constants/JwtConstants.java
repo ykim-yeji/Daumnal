111476f05a6ce061fce4fbe32c980386e794ae5e
core <jinjs6@gmail.com>
2024-03-15 14:41:25 +0900

    fix: jwt 상수에서 memberNickname 정보 삭제
    Former-commit-id: db3498ca325be8bcd26b5f6f300791fd76ecc7b2

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/JwtConstants.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/JwtConstants.java
@@ -5,5 +5,4 @@ public interface JwtConstants {
     String ISSUER = "daumnalUrl"; // 발행한 개체의 url이 들어간다.
     String ID_CATEGORY = "socialId";
     String PROVIDER_CATEGORY = "socialProvider";
-    String MEMBER_NICK = "memberNickname";
 }
