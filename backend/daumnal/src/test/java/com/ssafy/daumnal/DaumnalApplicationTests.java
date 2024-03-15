85c889233a4f4fa966d48a15ec29820bf59fc656
core <jinjs6@gmail.com>
2024-03-15 10:33:38 +0900

    feat: 첫 로그인 여부 필드 추가
    Former-commit-id: 8c88c4f2ef49902f2063f122342a7e32355a274b

--- backend/daumnal/src/test/java/com/ssafy/daumnal/DaumnalApplicationTests.java
+++ backend/daumnal/src/test/java/com/ssafy/daumnal/DaumnalApplicationTests.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9f6dab90224a4c71826f89b1a815c2655fff3fa79d4da41278818d1dfa21d347
-size 218
+package com.ssafy.daumnal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaumnalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
