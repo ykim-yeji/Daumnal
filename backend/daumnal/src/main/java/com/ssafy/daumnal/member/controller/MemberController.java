4eb7f1f288522e959f6851be84fcf5ca4b078ca3
core <jinjs6@gmail.com>
2024-03-15 11:14:10 +0900

    chore: 주석 내용 변경
    Former-commit-id: 0b9ce630d20f5eb572a816b3a57c628b95b4d6ab

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/controller/MemberController.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9e33fd37eddce4cb3507259a23e34094cdeba6ed73e04b02ab3f92b17e8de74
-size 1553
+oid sha256:0b0431255a87ff3e8d356cd7aa137bc1a459c13a6ddcdb240eb423d7db75ec0a
+size 1554
