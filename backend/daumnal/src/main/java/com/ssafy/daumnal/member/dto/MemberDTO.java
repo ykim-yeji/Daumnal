0e3f95bb57059cf6725861a8b96ab81dda886ec5
core <jinjs6@gmail.com>
2024-03-15 16:19:42 +0900

    feat: 로그인 response dto에 refresh token 정보 추가
    Former-commit-id: 8f28e00d75690d0566915da99eb162e9004ba771

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/dto/MemberDTO.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/dto/MemberDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50e77a9282bc5107a59ebbf5d76c2d2d28e043817007c9fb0b5f651452bac0da
-size 787
+oid sha256:bde99fb8957d5b2202ca91d2ac89d4e14b2479f8c92616ee7ad942effc8b87d3
+size 830
