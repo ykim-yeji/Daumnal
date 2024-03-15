d3c38bf1309519c2d8c7ef0a114fde4e05f33993
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-03-15 12:53:48 +0900

    fix: 캐릭터 이름 수정
    Former-commit-id: 2abe05bbccd6afe695acad720591f492a4e90ca2

--- frontend/daumnal/src/pages/LoadingPage.tsx
+++ frontend/daumnal/src/pages/LoadingPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:479f284664d3ad8fa446cd1fc74cd15f1994b0f5b50afa9c5e6f80e17440ccad
-size 1428
+oid sha256:5843ceb0f68c59aa4f82caa5f8ac6d2143acf6d80fb699ebe7e5099f32edcc6d
+size 1424
