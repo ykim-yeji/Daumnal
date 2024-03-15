a81a48935567dcded89a4c2da43bca192ff7d61f
core <jinjs6@gmail.com>
2024-03-15 10:57:36 +0900

    fix: jwt 토큰에 닉네임 정보 제거
    로그인 api 기능에 대한 변경에 의하여 jwt 토큰에 닉네임 정보를 제거하게
    되었습니다. 또한 redis key 값이 String으로 지정됨에 따라 memberId를
    String으로 변경하는 기능도 추가하였습니다.
    
    
    Former-commit-id: 935de321f04e8404b5199b3744e7dbd94c0271e4

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/util/JwtProvider.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/util/JwtProvider.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d006fe9e9926e1fa23c939f782c5aeb030b5861f7382a81f5fc0b5dc429e1386
-size 3133
+oid sha256:c752fc7c16933b25f10775edfa1b5e198fe979dbe27c90542deb539242d94560
+size 3016
