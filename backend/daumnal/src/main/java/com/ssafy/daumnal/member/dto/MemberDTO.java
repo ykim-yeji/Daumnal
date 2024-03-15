b07e4633a7f075b29c262fcd77cdee1183376791
core <jinjs6@gmail.com>
2024-03-15 11:12:38 +0900

    fix: 필요 없는 dto 삭제
    회원가입 api, 특정 회원 정보 조회 api가 삭제됨에 따라 그와 관련된 dto를
    삭제처리하였습니다.
    
    
    Former-commit-id: 51ca1bd596132c6b8522be80df6c384e83a951cb

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/dto/MemberDTO.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/dto/MemberDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42b335c85a6ed4aeb9b0b8b9f5c30568096de031f44fe576b60703b519770c10
-size 987
+oid sha256:f0224b13f8ef1088683fd8820eaefbbf8a6b2ef138d7ad5e3f45494fd9195b86
+size 613
