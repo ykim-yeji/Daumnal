1446a5060597f0d95bf3d716867a7d077514a52b
김예지 <ilice0403@naver.com>
2024-03-18 17:44:42 +0900

    fix: release branch push를 위해 db 주소 변경 - application.dev.yml
    Former-commit-id: b4709a99597f10e0e9e6547bda00a5a3540c9e9d

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/repository/DiaryRepository.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/repository/DiaryRepository.java
@@ -1,9 +1,3 @@
-package com.ssafy.daumnal.diary.repository;
-
-import com.ssafy.daumnal.diary.entity.Diary;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DiaryRepository extends JpaRepository<Diary, Long> {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:311492d7e49799f10cbd098134c0af85b46426deb4887edc39f569ef3fb62677
+size 286
--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/DiaryService.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/DiaryService.java
@@ -1,4 +1,3 @@
-package com.ssafy.daumnal.diary.service;
-
-public interface DiaryService {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:49531dcc7c7e5a6b2573f56a7b6562b0ce24e456eac3ad4265cad4a1d4297dc6
+size 75
