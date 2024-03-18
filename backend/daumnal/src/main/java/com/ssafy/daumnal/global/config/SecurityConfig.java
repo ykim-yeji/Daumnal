519f6d1d275f16e5289dd252d7a6b8a857e6359b
core <jinjs6@gmail.com>
2024-03-18 16:41:45 +0900

    fix: cross origin 문제 해결
    Former-commit-id: d2070c3533430d3ba166b320ccd6cebc3c1dd81a

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/config/SecurityConfig.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69ca149739be995a734a8545535459c5bf1b90aed7f47fdc53095ce33bab6fd8
-size 2164
+oid sha256:3eecf9928d5f13c4ebac97d64c5eee3fedade8c1fed81072ea9e473b0fe4efae
+size 2803
