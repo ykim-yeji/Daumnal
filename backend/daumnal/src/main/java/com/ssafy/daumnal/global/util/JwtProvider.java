67405a371b47ab8d2f45329e20995b9651e365c7
core <jinjs6@gmail.com>
2024-03-18 15:22:35 +0900

    chore: redis 관련 내용 잠시 주석 처리
    Former-commit-id: 0f56537e4074b8ce8b0a891d77e22c86ef590efb

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/util/JwtProvider.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/util/JwtProvider.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc487de054483507a5ed258eb6e751f70554ff2a88a79312e174ffe0ec76348d
-size 4549
+oid sha256:5b549266ee20ab2a0be721fa8c50a8f0f23b18ec5f62c6410e5d1a5a43ff870a
+size 4525
