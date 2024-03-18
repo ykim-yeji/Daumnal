182928927b964499da7c42767c3b0f68a80c790c
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-03-18 14:03:13 +0900

    fix: 노래 재생 컴포넌트 디자인 수정 완료
    Former-commit-id: f25ab008eadba5b438dcfe215171a19814054d18

--- frontend/daumnal/src/components/music/MusicPlay.tsx
+++ frontend/daumnal/src/components/music/MusicPlay.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17c68cd895ee2cb140ae28bad38a78110493b5b78990b1825fad550d878a338a
-size 6330
+oid sha256:75f7f633985a96ceded015854e18ed807652cd36d7d9b95b0654cb4b64732f72
+size 6799
