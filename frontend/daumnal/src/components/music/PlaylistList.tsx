0e70c4f8af76740f1f271328dc90ca0ffdbe8fa3
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-03-18 17:16:55 +0900

    fix: 노래 재생 기능 수정 완료
    Former-commit-id: 525cf5b51bf3b12cef5461dc69b885d3f0e0b3de

--- frontend/daumnal/src/components/music/PlaylistList.tsx
+++ frontend/daumnal/src/components/music/PlaylistList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6dd8d75e03d9ae6e286ea7068d37f27f7e2aff0624e432239d029dbc077eeba4
-size 2363
+oid sha256:d074453ced52974ff24d5eda6e2c13d10e684f686f1e88e42c91e4b749604fdf
+size 2434
