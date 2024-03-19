2bcfde30ca935845b22edd4c91bf6ccc37caff64
core <jinjs6@gmail.com>
2024-03-19 12:26:19 +0900

    chore: api 설명 주석 추가
    Former-commit-id: c9900c3f40a8ff4a67104f0183fe5d76889e41c9

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/controller/MemberController.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9f2294ab46d0a985694b1aab698ee53475955d8b10f1b6f101c371ad2ea5449
-size 2511
+oid sha256:9e5d171c94b2d611da42672f9ada68453f3e53aae5b2da7867a42e2979f26188
+size 2595
