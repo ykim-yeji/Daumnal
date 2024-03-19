43ca9f4c743b07d016652d3fa811273447d55da3
core <jinjs6@gmail.com>
2024-03-19 15:49:51 +0900

    feat: 일기 입력 request dto 생성
    Former-commit-id: 308cb484b945a623b42d2142e0c6f862558dbb0d

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/dto/DiaryDTO.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/dto/DiaryDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3221d73b48b3ac37df881c03d0fced2e6ef4926a6c17c42f771391abd36bd15
-size 238
+oid sha256:d4a9c755971ca983c35f52d5d5c099c7035c2f3e0027714e795ca3d31422fd5a
+size 846
