8eebe8d69266705360c1aa69cc3bcaa268b120ee
core <jinjs6@gmail.com>
2024-03-19 14:09:09 +0900

    feat: 일기 작성 여부 api 구현
    Former-commit-id: 9837ff78d051c203d689747e56c59428944aca49

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/repository/DiaryRepository.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/repository/DiaryRepository.java
@@ -1,9 +1,12 @@
 package com.ssafy.daumnal.diary.repository;
 
 import com.ssafy.daumnal.diary.entity.Diary;
+import com.ssafy.daumnal.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
+
+    boolean existsByMember(Member member);
 }
--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/DiaryService.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/DiaryService.java
@@ -1,4 +1,7 @@
 package com.ssafy.daumnal.diary.service;
 
+import com.ssafy.daumnal.diary.dto.DiaryDTO;
+
 public interface DiaryService {
+    DiaryDTO.GetDiaryWrittenTodayResponse getDiaryWritten(String memberId);
 }
