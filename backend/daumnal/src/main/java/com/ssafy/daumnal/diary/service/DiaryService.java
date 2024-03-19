fcf88488e119979d88649affec124b56f25635d4
core <jinjs6@gmail.com>
2024-03-19 14:09:09 +0900

    feat: 일기 작성 여부 api 구현
    Former-commit-id: 9837ff78d051c203d689747e56c59428944aca49

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/DiaryService.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/DiaryService.java
@@ -1,4 +1,3 @@
-package com.ssafy.daumnal.diary.service;
-
-public interface DiaryService {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:d1bd55c62b04d34ef5084d3ba71b360bee84fa63104abfe9efb5301218eaef68
+size 197
