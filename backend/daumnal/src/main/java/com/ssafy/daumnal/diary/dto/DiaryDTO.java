b745cbe3fff97eabaefd691678ec1210e334a264
core <jinjs6@gmail.com>
2024-03-19 15:49:51 +0900

    feat: 일기 입력 request dto 생성
    Former-commit-id: 308cb484b945a623b42d2142e0c6f862558dbb0d

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/dto/DiaryDTO.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/dto/DiaryDTO.java
@@ -1,14 +1,3 @@
-package com.ssafy.daumnal.diary.dto;
-
-import lombok.*;
-
-public class DiaryDTO {
-
-    @Getter
-    @Setter
-    @AllArgsConstructor
-    @Builder
-    public static class GetDiaryWrittenTodayResponse {
-        private boolean written;
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:d4a9c755971ca983c35f52d5d5c099c7035c2f3e0027714e795ca3d31422fd5a
+size 846
