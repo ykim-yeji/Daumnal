67af836932d9e53129a2b1d436b1d208746f8ace
김예지 <ilice0403@naver.com>
2024-03-19 17:54:50 +0900

    fix: master branch의 docker-compose.yml react Dockerfile 경로 수정
    Former-commit-id: dab1bdc4020764ca157f40778cc4059887798dc1

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/DiaryService.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/DiaryService.java
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d1bd55c62b04d34ef5084d3ba71b360bee84fa63104abfe9efb5301218eaef68
-size 197
+package com.ssafy.daumnal.diary.service;
+
+import com.ssafy.daumnal.diary.dto.DiaryDTO;
+
+public interface DiaryService {
+    DiaryDTO.GetDiaryWrittenTodayResponse getDiaryWritten(String memberId);
+}
--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/PageSize.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/PageSize.java
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:306a06cf6a30aab6a143b14eda452d619ebb124e79f9898ea5cbed0f2ffc8997
-size 70
+package com.ssafy.daumnal.global.constants;
+
+public class PageSize {
+}
