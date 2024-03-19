eb965c3645e99bada42c3fcd8389733012e3c1a5
김예지 <ilice0403@naver.com>
2024-03-19 14:35:39 +0900

    fix: s3 일기 내용 중 사진 업로드 메소드 수정
    - Diary 객체 삭제
      - 일기는 생성, 삭제만 있고 수정이 없기 때문에 이미 저장된 사진을
        삭제하는 로직이 필요가 없다.
    - S3Service delete 메소드 추가
      - 일기 사진 삭제 시 필요
    
    
    Former-commit-id: 594b56705cb7cebf81d8f75de85bf323420865c9

--- backend/daumnal/src/main/java/com/ssafy/daumnal/s3/service/impl/S3ServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/s3/service/impl/S3ServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eaaff64a2d94658ffb8bd4ba7da4a7ebe8eae5da77ba9784cfa34015ae40ea65
-size 4162
+oid sha256:95122303fd2e84ae98b80652e0e4529f232010cff527dd661b5bbcbb21c675d9
+size 3898
