c3130e8e1fcde55072a4611d34460e4f1defae4e
core <jinjs6@gmail.com>
2024-03-19 12:47:42 +0900

    chore: 닉네임 정보 변경 api 주석 설명 추가
    Former-commit-id: ac80af16851974b952f53b616abf8c68d7f65713

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/controller/MemberController.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fd11713357a2bcb30ba36793f4c70609c35ecad5ed990ca32e05d4572c8ffef
-size 3120
+oid sha256:2819b07aa04b2a200e3504b905b405ca0cff27a972b97b76e8a00049a3af34ec
+size 3245
