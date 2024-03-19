be6c5f81193ae6a222be6fd6c5fe9b306f735a86
김예지 <ilice0403@naver.com>
2024-03-19 17:26:33 +0900

    fix: docker-compose.yml 필요없는 부분 삭제
    Former-commit-id: b18bf52cf36d690e09d363b010994fb3952eae33

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/DiaryService.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/DiaryService.java
@@ -1,7 +1,3 @@
-package com.ssafy.daumnal.diary.service;
-
-import com.ssafy.daumnal.diary.dto.DiaryDTO;
-
-public interface DiaryService {
-    DiaryDTO.GetDiaryWrittenTodayResponse getDiaryWritten(String memberId);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:d1bd55c62b04d34ef5084d3ba71b360bee84fa63104abfe9efb5301218eaef68
+size 197
--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/MusicService.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/MusicService.java
@@ -1,4 +1,3 @@
-package com.ssafy.daumnal.music.service;
-
-public interface MusicService {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b3a0ccf92526b7ccd18c4b158e55a9982cd108c85d29b834aca14b322b2d56cd
+size 75
