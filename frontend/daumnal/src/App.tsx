6252cd384163a4b388828c4173d611015d3b82cb
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-03-19 10:10:29 +0900

    fix: 경로 변경
    Former-commit-id: 3f6b0c4951beb5df9a7676c8a88b6b6d44a71641

--- frontend/daumnal/src/App.tsx
+++ frontend/daumnal/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14efb254e1d2b9334c4dc52af86d7d13213db4b8e9dddb132787d7f3644593a0
-size 1729
+oid sha256:76f189255940a2be4323cb93414d0c4d91ea24b45d11a41e071f2fc0200e7771
+size 1723
