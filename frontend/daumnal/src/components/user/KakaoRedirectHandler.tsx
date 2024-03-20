5e50b9d3df31403725b741e8ac62ce51e47874ab
정성현 <gkgktjdgusdl@naver.com>
2024-03-20 17:44:44 +0900

    fix : redirect_url 수정
    Former-commit-id: 02b903c4f92323146f25b284834c7aba0e6cd90d

--- frontend/daumnal/src/components/user/KakaoRedirectHandler.tsx
+++ frontend/daumnal/src/components/user/KakaoRedirectHandler.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adc94608c37bf49ec03a7e9d9e3efbbb9343b1c058fea218df93e569bd454432
+oid sha256:018102ea4798137daabb2256de6751533cfb7ff6e45307c69a9f540d4c229051
 size 4349
