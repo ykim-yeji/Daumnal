68a6a9f2f291e02d2106e106a1b8977ad678737d
core <jinjs6@gmail.com>
2024-03-20 09:11:57 +0900

    chore: 메서드 카멜케이스로 변경
    Former-commit-id: 8bcc078a239d60b0a10b65eed91917b831c5c79f

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/controller/MemberController.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c8f83d8699f18581fe114b61fd299c331d713f923d15968dabadaf7c898fac3
-size 3715
+oid sha256:06ce9d620ce395bef8e3e19266a38ff89ee0c5d75ac017d43b6899599afbf1a9
+size 3739
