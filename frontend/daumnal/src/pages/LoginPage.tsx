6fe6032da870c4c6f6e5379894636c1f34636f11
정성현 <gkgktjdgusdl@naver.com>
2024-03-20 17:44:44 +0900

    fix : redirect_url 수정
    Former-commit-id: 02b903c4f92323146f25b284834c7aba0e6cd90d

--- frontend/daumnal/src/pages/LoginPage.tsx
+++ frontend/daumnal/src/pages/LoginPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f943298555abd3e30f5b3b14d0f369a3ed680fe940c7e81279e9ddc937fe234
-size 2006
+oid sha256:fd3e19294f27cb89555d84359450516d86343eae8c44ae7d8a5048cdb8debdc5
+size 2010
