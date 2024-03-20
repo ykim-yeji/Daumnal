6e6c91d02d979a8f6a6985549ffc2083e04f2da8
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-03-20 17:24:03 +0900

    feat: 플레이리스트 정보 관련 모달 구현
    Former-commit-id: b454d70f00715942e4444c4cbab65d411f71a9e2

--- frontend/daumnal/src/components/modal/CreatePlaylistModal.tsx
+++ frontend/daumnal/src/components/modal/CreatePlaylistModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:568bbf3971438177fb7139deb7ec9c41a38d4ad20da52f9c6583f7d2d6dac8ea
-size 5330
+oid sha256:523ddb92c76ef470f2938666933d62a51c04cf537bb170e714979cf227b11c73
+size 5278
