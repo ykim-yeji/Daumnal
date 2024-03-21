c208c1031f9c5896da44c5629a4715468ad2e7a8
core <jinjs6@gmail.com>
2024-03-21 13:35:53 +0900

    fix: Multipart 데이터 dto에 재추가 및 ModelAttribute 재적용
    Former-commit-id: aa83830619eb0274ad8479882802ac6af395e9f4

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/dto/DiaryDTO.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/dto/DiaryDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d391cecbc2de7185c71f1082c302429a0fbaccbc5431d417e88177f8dc10141d
-size 1324
+oid sha256:44d8ca97821af720bd37fc79820dea0f78ae901f29544b57fa0a817b0bc8397c
+size 1383
