a2ea751f50ce4328d01171c0cefcf00e20d6bcd1
core <jinjs6@gmail.com>
2024-03-21 14:23:13 +0900

    fix: 감정 정보 관련 타입 String 으로 변경
    클라이언트의 요청에 의하여 감정 정보 데이터의 타입을 String 으로 변경하였습니다.
    
    
    Former-commit-id: 34131531e19a739cb42873e025866e8ba354c281

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/dto/DiaryDTO.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/dto/DiaryDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44d8ca97821af720bd37fc79820dea0f78ae901f29544b57fa0a817b0bc8397c
-size 1383
+oid sha256:f8dfb2dc055faf8f0ab3b154a747b3b578c974f7ff7db19b57b6352fc964ff0f
+size 1376
