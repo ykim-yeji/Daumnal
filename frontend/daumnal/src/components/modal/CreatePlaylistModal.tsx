7c5443d4b1641a13252fb4cca3ed063aabbec45c
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-03-21 15:20:35 +0900

    fix: 모달 디테일 수정
    Former-commit-id: 36529b447885df235dd60351ff82e065868d2ea3

--- frontend/daumnal/src/components/modal/CreatePlaylistModal.tsx
+++ frontend/daumnal/src/components/modal/CreatePlaylistModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:523ddb92c76ef470f2938666933d62a51c04cf537bb170e714979cf227b11c73
-size 5278
+oid sha256:7c6741f945f6a50630a92ef92fbfea40c3b28d4658f999381e8effc84ce181ff
+size 5329
--- frontend/daumnal/src/components/modal/LyricsModal.tsx
+++ frontend/daumnal/src/components/modal/LyricsModal.tsx
@@ -1,3 +1,2 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2e04f7c33915f6ed23e793952a97cbe19890beddb1a37d9d7a780f6e9f2071c0
-size 27
+// 가사 모달
+export {};
