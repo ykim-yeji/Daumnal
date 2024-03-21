8da80b160759e0ac424b06fb992392b81986b08d
core <jinjs6@gmail.com>
2024-03-21 12:59:17 +0900

    fix: 일기 입력 dto 형태 수정
    이미지는 json 형태로 처리를 하지 못하기 때문에 json 입력 전용 공간인 글
    내용을 입력 받는 공간으로 변경하였습니다.
    
    
    Former-commit-id: d04a2e516b94998efe5f58ff36bae2deb2abff9f

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/dto/DiaryDTO.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/dto/DiaryDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c7b2df9e62d19330f0d21ee5c4411a9ad71eee27ebbe897db224f2dd02d7b63
-size 1359
+oid sha256:d391cecbc2de7185c71f1082c302429a0fbaccbc5431d417e88177f8dc10141d
+size 1324
