825ea9396a39c417cad97512b63c7f0e849435ee
core <jinjs6@gmail.com>
2024-03-21 17:28:30 +0900

    style: 코드 수정
    서버 배포 문제가 해결됨에 따라 각 컨트롤러마다 적용된 Cross Origin
    어노테이션을 제거하였습니다.
    그 외 로그용 System.println도 제거해주었습니다.
    
    
    Former-commit-id: 93b5440f57ce78ad707f549d43c4b6d0dbf18f13

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/controller/PlaylistController.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/controller/PlaylistController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58b940929d90a89b0acae05136e3cd0bc9d68683f0bd1926c5684f22b9b5e720
-size 507
+oid sha256:95eeba54ede41e694f4bb33976ebb70c3bbc75c44cc258524671cd74845d3300
+size 429
