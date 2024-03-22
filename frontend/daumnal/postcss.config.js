36f006edb566b82e64f2f34637b9faf88baff347
김예지 <ilice0403@naver.com>
2024-03-22 13:21:58 +0900

    fix: ErrorCode에서 INVALID_INPUT_DATA 삭제 : 필요없음
    Former-commit-id: ed78c84649d6a252cf1ec54fa568e186011676c5

--- frontend/daumnal/postcss.config.js
+++ frontend/daumnal/postcss.config.js
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e593f311f4c2c148cd827fd1f06e19f4ca83fad609830539803e2e8e78307da7
-size 70
+export const plugins = {
+    tailwindcss: {},
+    autoprefixer: {},
+};
