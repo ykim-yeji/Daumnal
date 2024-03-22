82e376647743ff60cd6c98f1be535045650f0bb3
김예지 <ilice0403@naver.com>
2024-03-22 13:21:58 +0900

    fix: ErrorCode에서 INVALID_INPUT_DATA 삭제 : 필요없음
    Former-commit-id: ed78c84649d6a252cf1ec54fa568e186011676c5

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/ErrorCode.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/ErrorCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fea4d5ad282e0f7a6189008292547bec6f84de50077e145e1433d15ed800986
-size 4301
+oid sha256:a657d70204ec4cf0a68c17bec7139f8181cc996de3c5d4f1c1c891cadd92ead7
+size 4206
