47641aefa6484147f57f22d9aa4e95208b29cc67
core <jinjs6@gmail.com>
2024-03-22 11:09:27 +0900

    fix: 닉네임을 입력 안했을 때의 예외처리 부분 수정
    null 뿐만 아니라 공백도 닉네임을 입력 안한 것이므로 둘 다 동시에
    처리하도록 기능을 변경하였습니다.
    
    
    Former-commit-id: b475275f8d3ab6984147ab36577482d5c4c97960

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/util/MemberUtilService.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/util/MemberUtilService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c305007a0d1d39e65f5565295abb6f02282a010b4150477896252ef04cdde59b
-size 4482
+oid sha256:091d416a68834a71af94ff3c47a033090dbc6a44a702025b844efdf2d5c2462a
+size 4496
