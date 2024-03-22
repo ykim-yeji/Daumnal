ba082f3b87841da8920d456e9cb6970004a24507
core <jinjs6@gmail.com>
2024-03-22 13:04:05 +0900

    feat: 특정 회원이 썼던 일기 목록 가져오기 기능 구현
    Former-commit-id: 9290febf273dfe0100227b4376273eb867db4b14

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/repository/DiaryRepository.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/repository/DiaryRepository.java
@@ -5,8 +5,12 @@ import com.ssafy.daumnal.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
     boolean existsByMember(Member member);
+
+    List<Diary> findDiariesByMember(Member member);
 }
