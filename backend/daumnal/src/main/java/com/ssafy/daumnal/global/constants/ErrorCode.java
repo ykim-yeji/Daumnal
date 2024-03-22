730b6de9fa7dcffb2e0d4070f9c81e2681329952
core <jinjs6@gmail.com>
2024-03-22 11:08:26 +0900

    fix: NOT_EXISTS_MEMBER_NICKNAME_INIT_GET에 관한 에러코드 403으로 수정
    Former-commit-id: b279131069a11a6957805c55bc1b747a3d4e653f

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/ErrorCode.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/ErrorCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84a9071bd99662a6b58732174c9adf72a9f907cfc51340920aaf524576bdc08b
-size 4116
+oid sha256:6cfc21a7ca84960939e4bb41aab6b81ad0615eeb2b09e342147ab4e37de1e12f
+size 4114
