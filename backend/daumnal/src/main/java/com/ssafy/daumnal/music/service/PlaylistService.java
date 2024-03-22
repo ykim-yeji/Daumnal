f9ba7e58b48c17664dad841b61f4370e60771013
core <jinjs6@gmail.com>
2024-03-22 16:57:36 +0900

    fix: 변경 nickname 및 originNickname null 검증 오류 해결
    Former-commit-id: f59b72f1f6d8add60857d8c2a991a87edf172362

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/PlaylistService.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/PlaylistService.java
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d58f00220b545498189c2a882acb9e1d5c5210c716791eaf7379b2219ab703b8
-size 218
+package com.ssafy.daumnal.music.service;
+
+import com.ssafy.daumnal.music.dto.PlaylistDTO;
+
+public interface PlaylistService {
+
+    void addPlaylist(String memberId, PlaylistDTO.AddPlaylistRequest addPlaylistRequest);
+}
