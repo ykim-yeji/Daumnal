c99b993c10558a13b80375692dbe5f4c732c2f11
core <jinjs6@gmail.com>
2024-03-22 15:28:24 +0900

    fix: 일기 내용 길이 입력 관련 오류 문구 수정
    Former-commit-id: 0e146dce5962577d700fabe6d0cf19002be2c180

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/ErrorCode.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/ErrorCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ab454698634306b2b58c2e999411f137b3ec3ae2a7c1ef949c63e7dd26c867c
-size 4500
+oid sha256:173414ae1ab181ef4bbbf3ee046ffbb495cf141be0429bbb4f2ca8e1a5340bcc
+size 4513
