8191e6a4d3e3521a52e12f39597178dcf34c266b
김예지 <ilice0403@naver.com>
2024-03-22 13:16:25 +0900

    feat: Controller Method 파라미터에서 타입에러 발생 시 예외 처리
    - GlobalExceptionHandler
      - handleMethodArgumentTypeMismatchException 메소드 추가
        - 타입 에러가 발생한 필드 정보를 메시지에 담아 응답한다. (필드 타입,
          사용자 입력 값, 필드 이름)
    - ApiResponse
      - error 메소드 추가
        - HttpStatus 타입 변수와 String 타입 메시지 변수를 받아 응답 객체를
          생성한다.
    - ErrorCode
      - INVALID_DIARY_ID 삭제
        - 필요없음
    
    
    Former-commit-id: 3250e820c213275710bb293791c23968515f5e4a

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/config/GlobalExceptionHandler.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/config/GlobalExceptionHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08f567fb55c44fb4c5fe0e3e48660b5e80cb5044ca05ff90398fc4d35bd0b075
-size 1114
+oid sha256:b849b724708d675800c9d3ff7e1b804ee671ecb055f2d9082a7fbc4e499d9b4e
+size 1679
