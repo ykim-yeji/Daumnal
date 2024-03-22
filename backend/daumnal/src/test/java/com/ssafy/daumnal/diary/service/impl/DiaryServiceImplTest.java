0fa0f357c52fa1e78f8a30693163b974fe978b15
core <jinjs6@gmail.com>
2024-03-22 14:41:28 +0900

    test: 일기 최신순 정렬 검증 테스트 코드 작성
    Former-commit-id: 93806b51e0e96f34d12809f166a8060a10a0fc2d

--- backend/daumnal/src/test/java/com/ssafy/daumnal/diary/service/impl/DiaryServiceImplTest.java
+++ backend/daumnal/src/test/java/com/ssafy/daumnal/diary/service/impl/DiaryServiceImplTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68d3749efd51cc442e63faa3ba0aaa5beb519d8cf1db7e4ac949a11622198bdd
-size 2064
+oid sha256:e9bcd236135a6e11b223d8062ac81c0e8dbc3b59ad901723c6572b268dc5283d
+size 3826
