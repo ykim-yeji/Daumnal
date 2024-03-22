909614e57681a686784f0af51d8917fd56356ab1
김예지 <ilice0403@naver.com>
2024-03-22 17:57:05 +0900

    feat: 플레이리스트 목록 조회 API 기능 구현
    - PlaylistController
      - getPlaylists 메소드 생성
    - PlaylistService
      - getPlaylists 메소드 생성
    - PlaylistServiceImpl
      - getPlaylists 메소드 생성
        - 회원 검증 기능
        - 플레이리스트 목록 페이지네이션 기능
    - SuccessCode
      - GET_EMPTY_PLAYLISTS, GET_PLAYLISTS 성공 메시지 추가
    - PlaylistRepository
      - findByMember 메소드 생성
        - 플레이리스트 목록 페이지네이션 기능
    - PlaylistDTO
      - GetPlaylistResponse와 GetPlaylistsResponse 추가
    - Playlist
      - toGetPlaylistResponse 메소드 추가
        - Playlist Entity를 toGetPlaylistResponse DTO로 변환
    - PageSize
      - PLAYLIST_LIST_SIZE 추가
    - PageResponse
      - 생성자에 @Builder 추
    
    
    Former-commit-id: fbe7632ad6d7f22b1e77d2d762b698285286df7b

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/PageSize.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/PageSize.java
@@ -1,4 +1,6 @@
 package com.ssafy.daumnal.global.constants;
 
 public class PageSize {
+
+    public static final int PLAYLIST_LIST_SIZE = 9;
 }
--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/Playlist.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/Playlist.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81a8aec3b286bea3a6c8e76555f36334674ef1b0a6ff4d09f704f7b393e7005f
-size 1334
+oid sha256:01450726efc4451cfe0dec6af8e2e88c5a716052e1c3061a9eb9c175c5440cb6
+size 1813
