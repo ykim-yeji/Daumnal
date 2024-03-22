8276839bd07181f7d47c26004780d0c13c48aef8
김예지 <ilice0403@naver.com>
2024-03-22 15:28:46 +0900

    feat: 플레이리스트 추가 API 기능 구현
    - PlaylistController
      - addPlaylist 메소드 생성
    - PlaylistService
      - addPlaylist 메소드 생성
    - PlaylistServiceImpl
      - addPlaylist 메소드 생성
        - 회원 검증 기능
        - 플레이리스트 커버 요청 유무에 따른 로직 구현 (플레이리스트 커버
          요청 없을
          시 기본 커버 주소 삽입)
    - S3ServiceImpl
      - uploadPlaylistCover 메소드 수정
        - 기존 플레이리스트 커버 삭제 조건문에 playlist != null 추가 :
          플레이리스트 첫 생성 시 Playlist 객체가 없는 것을 고려
    - SuccessCode
      - CREATE_PLAYLIST 성공 메시지 추가
    - Playlist
      - coverUrl 변수의 기본값 변경
      - @Builder 있는 생성자 2개 추가 : 플레이리스트 추가 시 사용
    - PlaylistDTO
      - AddPlaylistRequest 생성
        - playlistName 변수는 @Valid 이용해 유효성 검증
          - null인지 여부
          - 글자 수 제한
        - toEntity 메소드 2개 생성 (플레이리스트 커버 유무에 따라 사용)
    - GlobalExceptionHandler
      - handleMethodArgumentNotValidException 메소드 생성
        - @Valid 유효성 검증에 따른 예외 처리 메소드
        - HttpStatus, 커스텀 예외 메시지, 입력값 정보 응답으로 반환
    - DiaryServiceImpl
      - addTodayRecommendedMusic 메소드의 주석 수정
    
    
    Former-commit-id: f84959ab0064fe48848a37cb4ddc5a68843ad952

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/Playlist.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/Playlist.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d9b106836896ffae8d6a654e44391ebd08b17bc3cec9c1d9a4a762ea532ed40
-size 1003
+oid sha256:81a8aec3b286bea3a6c8e76555f36334674ef1b0a6ff4d09f704f7b393e7005f
+size 1334
