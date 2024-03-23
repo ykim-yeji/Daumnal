e8bdab5d7b491be78df2fbe81be5c6b12b4fc65a
ykim-yeji <ykim5867@gmail.com>
2024-03-23 17:37:36 +0900

    feat: 플레이리스트 정보 조회 API 기능 구현
    - PlaylistController
      - getPlaylist 메소드 생성
    - PlaylistService
      - getPlaylist 메소드 생성
    - PlaylistServiceImpl
      - getPlaylist 메소드 생성
        - 회원 검증 기능
        - 플레이리스트 검증 기능
        - 플레이리스트 정보 조회 기능
    - SuccessCode
      - GET_PLAYLIST 성공 메시지 추가
    
    
    Former-commit-id: 0a2a9b77d58a16efec4ac07e030e5e1de5e2f2c7

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/PlaylistService.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/PlaylistService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5100ba962a21c4767d83b8f362b051c298c1ae10753ec59316701e25166eaf6
-size 576
+oid sha256:8bd06ba28b1cc1f30b496f201ff181448a0c3e1175989e0a77ee179df30934cf
+size 555
