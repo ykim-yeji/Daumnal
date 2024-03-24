eb3f7a3d599e35d7f5c361ccf25807cd80678a9e
core <jinjs6@gmail.com>
2024-03-24 19:04:37 +0900

    feat: Jwt 커스텀 에러 구현
    Jwt 인증 오류 발생시 클라이언트에게 에러 내용을 반환하기 위한 설정을 진행했습니다.
    클라이언트에게 오류 내용을 반환하기 위한 핵심 인터페이스는 AuthenticationEntryPoint 이므로 여기에 저의 CustomAuthenticationEntryPoint를 만들어서 오류 내용을 response에 담았습니다.
    만약 CustomAuthenticationEntryPoint 가 없다면 spring security 필터에서 기본적으로 Http403ForbiddenEntryPoint (AuthenticationEntryPoint를 implement) 를 활용하는데 여기서 기본 오류 내용을 response.sendError()를 통해서 오류 처리를 하기에 적절한 에러 내용을 반환을 하지 못하기 때문에 CustomAuthenticationEntryPoint를 만들게 되었습니다.
    또한 JwtAuthenticationFilter에서도 catch 부분을 추가하였는데 그 이유는 request에 INVALID_MEMBER_TOKEN을 CustomAuthenticationEntryPoint로 보내주기 위함입니다.
    마지막으로 SecurityConfig에 저의 CustomAuthenticationEntryPoint를 추가함으로써 스프링 빈에 등록하여 에러 처리를 진행할 수 있도록 하였습니다.
    
    
    Former-commit-id: b9e4e9e74e0c150f25874f2cdaecb5b7b3b2c562

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/util/JwtAuthenticationFilter.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/util/JwtAuthenticationFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89fc94763cf7381ed3dd6a8cc9d090fef8ebec90f1b5731ae7111f6d4b138e2c
-size 2059
+oid sha256:5e2632c1e95267535f280febea4fdacdea893b51d7a24adda4802262e43750f1
+size 2576
