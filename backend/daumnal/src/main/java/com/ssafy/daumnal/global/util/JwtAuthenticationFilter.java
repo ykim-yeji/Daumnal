b30356e7e2d0a58db6752ac7adfc4b3b04bac415
core <jinjs6@gmail.com>
2024-03-24 19:47:39 +0900

    fix: access token 입력 대신에 refresh token을 입력 시 출력 되는 예외 내용 변경
    access token을 통해서 인증을 해야 하는 api에 refresh token을 입력하는 경우 "서버 오류 입니다!" 문구로 출력이 되도록 설정이 되었는데 "유효하지 않은 토큰입니다!"로 출력하도록 설정을 바꿨습니다.
    던지는 예외로 JwtException을 활용하였으나 추후에 수정이 필요한 부분이 생기면 수정할 예정입니다.
    
    
    Former-commit-id: b1e872341d463cc734226a6c7a558d243080144c

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/util/JwtAuthenticationFilter.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/util/JwtAuthenticationFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa3e1ed3bad330740918859a279ff1e761428b4053960bdba16770cac07523af
-size 2391
+oid sha256:dd92c1acf6c6a55b7f9e898bbd7332b4633b0649a2b199284283390c4ab994d0
+size 2392
