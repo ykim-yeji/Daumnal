d149941f9a2f4555dada3e594cc3690461fede69
core <jinjs6@gmail.com>
2024-03-25 17:48:24 +0900

    feat: 일기 삭제 api 구현
    Former-commit-id: 469bc6104a0d9858cab0daf203fb56f0111c43e5

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/impl/DiaryServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/impl/DiaryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04cefd326a9012ecd39285dbba40d304f8c8e2e625a3e841041fef95f9cddbde
-size 11562
+oid sha256:1eb58dedff1cc1e7d3c62b68c200a4be908b806333e42e251935aa41b8b35503
+size 12427
