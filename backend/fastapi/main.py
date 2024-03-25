363f134a8d57a18b32b3c66fb4ba00a3ec2b59d8
전새벽 <wjswwkd@naver.com>
2024-03-25 11:27:59 +0900

    feat: 추천 기능 구현
    Former-commit-id: 6e4158c4d709f872947b0bfb259bc6f27b1f6938

--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b15b2358833fec491c7b65cb842ef4926ae6a222fdc481bcd59321972ea46621
-size 1843
+oid sha256:67fb558102067a7ddd74a364ae8b7a926f6a608a596a5909496c7c68f5efe359
+size 3397
