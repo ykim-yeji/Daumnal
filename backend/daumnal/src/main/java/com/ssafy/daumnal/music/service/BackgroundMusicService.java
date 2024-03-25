776de6a47cffafdffa606b0947f0cef941d6271f
core <jinjs6@gmail.com>
2024-03-25 20:45:43 +0900

    feat: 배경음악 정보 변경 api 구현
    Former-commit-id: 514e01d9aec11ff20d61419836692ebb8282c390

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/BackgroundMusicService.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/BackgroundMusicService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7830b5649db9346bea49e804ceb685bc7e4569718bcf9fb1c599acd8668a4ae8
-size 418
+oid sha256:170377a44c4a9dbd8878264b585a5e3dd22eb581877b887a3b8cd96f7fe5ed4e
+size 516
