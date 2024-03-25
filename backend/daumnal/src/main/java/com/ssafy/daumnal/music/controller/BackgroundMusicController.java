d0ae4f0b4cb6eff56171c9b9cd9609b7752d46c1
core <jinjs6@gmail.com>
2024-03-25 20:47:56 +0900

    style: 주석 설명 추가
    Former-commit-id: 68e9d7253715c0bc1dd8ea4c25d3885d59e02d89

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/controller/BackgroundMusicController.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/controller/BackgroundMusicController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e96bb86be725eb98d8909985e2b72102df2044932cb075ce6af2f8ca65968819
-size 1964
+oid sha256:c86a5f36fe73f44745d3728aae570ebcad4938148cb52ef319c619de177aa73e
+size 2095
