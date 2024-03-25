c8cf52366652b8de908602e063811011abe388f0
core <jinjs6@gmail.com>
2024-03-25 13:59:22 +0900

    feat: 잘못된 일기 id 입력에 대한 예외 문구 추가
    Former-commit-id: 2042c883155d3422e4d599095b18d917ab86fbbc

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/ErrorCode.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/ErrorCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b6ec530fa70af0336b72b51e6c2766ecee579dbdeabb5278433cf4d62cba6b0
-size 5103
+oid sha256:8e43a2df2e2efca86bfadc5ec12367c3bce76c9af84efba3144d02cf46fb4ddf
+size 5195
