bf9199ff345300b9a848f1f233233c49f8a2f80f
jojoo <csh990720@gmail.com>
2024-03-25 17:45:58 +0900

    fix: 일기 내용 전송 시 문장 구분 기능
    Former-commit-id: acd4a376c932e3ba036a613e20ef034a04fc0466

--- frontend/daumnal/src/pages/CreateDiaryPage.tsx
+++ frontend/daumnal/src/pages/CreateDiaryPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f6f3c6f54fb53f8f71f0967885fe3c9d02a5a52e99cd7b1583b640112740dfb
-size 5592
+oid sha256:0f36f9933094ac20312d5a92dda18b52a193aa9a699f266d832cee78a06fb66c
+size 5655
