88c977829fe4db79c55bc69130f648e7d0412e1b
jojoo <csh990720@gmail.com>
2024-03-25 10:34:44 +0900

    feat: 일기 입력 3000자 초과시 등록 제한 기능
    Former-commit-id: da3b19eea88b9af8bbe24f7079574d840e88e486

--- frontend/daumnal/src/pages/CreateDiaryPage.tsx
+++ frontend/daumnal/src/pages/CreateDiaryPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba6fc330ec4a12c82947d19e2fdb063268e06e05c9caacb1a01764d589bf3ca5
-size 5385
+oid sha256:6f6f3c6f54fb53f8f71f0967885fe3c9d02a5a52e99cd7b1583b640112740dfb
+size 5592
