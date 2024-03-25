7678264d227c745bc8a1776f99f15b98ccc98689
core <jinjs6@gmail.com>
2024-03-25 19:24:06 +0900

    refactor: dto 명칭 변경
    GetBackGroundMusicResponse 를 선택한 배경 음악 조회에 활용을 할 것이므로
    컨벤션에 맞게 변경하였습니다.
    
    
    Former-commit-id: f28799aa28b3b6ab4f2f9a99af6e67b2fec4e919

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/BackgroundMusicService.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/BackgroundMusicService.java
@@ -1,7 +1,7 @@
 package com.ssafy.daumnal.music.service;
 
-import com.ssafy.daumnal.music.dto.BackgroundMusicDTO.GetBackgroundMusicResponse;
+import com.ssafy.daumnal.music.dto.BackgroundMusicDTO.GetBackgroundMusicsResponse;
 
 public interface BackgroundMusicService {
-    GetBackgroundMusicResponse getAllBackgroundMusic(String memberId);
+    GetBackgroundMusicsResponse getAllBackgroundMusic(String memberId);
 }
--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/impl/BackgroundMusicServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/impl/BackgroundMusicServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8224eca5a48a307cd2e4e21f2d7a46ff07fdcad47ef7eac20be721dc8072c39
-size 2281
+oid sha256:98b02d684096bbd4b9ac55002ec6e5bdbed195a02b65e1106156096a09fcb2a8
+size 2317
