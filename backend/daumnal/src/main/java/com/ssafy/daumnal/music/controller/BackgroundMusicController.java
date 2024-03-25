ebf7f38d54dff38e3b3b248eb5e82e105f8ce446
core <jinjs6@gmail.com>
2024-03-25 20:27:45 +0900

    style: 주석 설명 추가 및 필요 없는 코드 제거
    Former-commit-id: 09ad177a654402713fd64ae45f78f8b3d1c7a63c

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/controller/BackgroundMusicController.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/controller/BackgroundMusicController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6193f8f223a7babcfdcc1a116acb4e0c837ee61712c22a437cde828d73a19a7
-size 1805
+oid sha256:081a3a970a0afab981d77adf74daa1cf1763e94d07664e19a3a9f1e9aa505f92
+size 1863
