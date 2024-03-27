f0d7c8e911ca41a35cc49baabdb6fd54d3cbb44e
전새벽 <wjswwkd@naver.com>
2024-03-27 15:34:19 +0900

    feat: 크롤링 기능 및 api 테스트 추가
    Former-commit-id: 990edc8ff394cd4e94e82739eccd80df086304d8

---
+++ backend/fastapi/crawl_test.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3e62a094b5039a483a02a4a3a1537a4159d9b95a2ec78d302a4875571da04a03
+size 5009
--- backend/fastapi/model.py
+++ backend/fastapi/model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc8d28837e70b5323e682e880fb4a24f1ebfea37d3c0aa232bb9a395ff7654dc
-size 1773
+oid sha256:55aa6cb396bea6ab8b0bcd1a58f46d7d6e38ab10bed055a3681e6f54daa45148
+size 1793
