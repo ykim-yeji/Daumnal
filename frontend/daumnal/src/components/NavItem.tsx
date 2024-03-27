75d0e852936255b6d38e7194b564665949751455
정성현 <gkgktjdgusdl@naver.com>
2024-03-27 16:22:41 +0900

    feat : 일기 브금 구현, 일기 작성 여부에 따른 동작 구현
    Former-commit-id: 767cb41a39cb3ba39a63ccffa4e03429b81e847d

--- frontend/daumnal/src/components/NavItem.tsx
+++ frontend/daumnal/src/components/NavItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d541eaaa004320cbcadd64c868dc5f75c7643935e8f36018c9deaf9a9543baa
-size 1138
+oid sha256:c3c952706a1b92269a947711dedfff337aa1b37a4fd23d900a23e9df44d5f489
+size 1952
--- frontend/daumnal/src/pages/LoginPage.tsx
+++ frontend/daumnal/src/pages/LoginPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd3e19294f27cb89555d84359450516d86343eae8c44ae7d8a5048cdb8debdc5
+oid sha256:1f9d84ca3605f3d92238ee9fa446d84b3e4a5e86940e2173e298d4f40b4e4b97
 size 2010
