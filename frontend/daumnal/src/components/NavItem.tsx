76f869c97c91461d9ccbb94d732a0cacb12970c0
정성현 <gkgktjdgusdl@naver.com>
2024-03-28 17:57:46 +0900

    feat : 네비게이션 세부 화면 조정, 로그인 및 닉네임 설정 예외 처리
    Former-commit-id: a2b11025957fb89d94719ebedbac0074dbbd9ef0

--- frontend/daumnal/src/components/NavItem.tsx
+++ frontend/daumnal/src/components/NavItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3c952706a1b92269a947711dedfff337aa1b37a4fd23d900a23e9df44d5f489
-size 1952
+oid sha256:154e1913b9889856cb7d50d9e548a59a89bc891ce1166db4d8ed02f5b056b8d9
+size 2189
