59ebbd2c59b1dacfb712105519b7beae4dac8120
전새벽 <wjswwkd@naver.com>
2024-03-28 14:05:42 +0900

    feat: 크롤링 기능 구현
    Former-commit-id: 49c0e4331dc3adb177b4b95ab24da355f68beb30

--- backend/fastapi/crawl_test.py
+++ backend/fastapi/crawl_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e62a094b5039a483a02a4a3a1537a4159d9b95a2ec78d302a4875571da04a03
-size 5009
+oid sha256:697020ac37ef499124fecf7f9f86aa539d7c25b9a0b98c0ca330ad051b974476
+size 5347
