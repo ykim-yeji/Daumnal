fdedc81b0a1697ffbfbb458d09d38890fe920bd1
core <jinjs6@gmail.com>
2024-03-28 17:49:36 +0900

    feat: 추천 노래 정보 조회 성공 문구 작성
    Former-commit-id: 7cd5d2a1d3f3a59b5cc1e889fcdfaed1e4e1cd99

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/SuccessCode.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/SuccessCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0f651b838399e4e0bbad550bb04c6045f2a2840ef0fc708fade73a72ab1b6cf
-size 3891
+oid sha256:623933e4fbaf24b18c9fe394ecfbe5f68f6bbfefcbf751b86800be7233fdc5c4
+size 4001
