c131573ef0afc7f582140f6071f3ca278d7e2191
core <jinjs6@gmail.com>
2024-03-28 17:21:05 +0900

    feat: 음악 정보 리스트 dto 추가
    Former-commit-id: c0f5639b63a90f97ba9a9b7d3547a2e46274d4e7

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/dto/MusicDTO.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/dto/MusicDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea3605a9e0d3f04077e329b0d0cd71bb2183a4e3681b6da3ff4cb25416aae010
-size 1607
+oid sha256:ef0f8cb0c99b6191f0fb3a425b00e687c851b06726f638a3c48c37c7d427646e
+size 1734
