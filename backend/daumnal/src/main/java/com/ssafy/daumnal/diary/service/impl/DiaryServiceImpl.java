b669ba1a542231e1bc3eb8275a7571af79e7b492
김예지 <ilice0403@naver.com>
2024-03-28 13:51:47 +0900

    fix: 좋아하는 가사 미선택 상태에서 가사 정보 조회 시 발생 에러 해결
    - DiaryServiceImpl
      - getLyricsOfTodayRecommendedMusic 메소드 수정
        - 좋아하는 가사 선택 번호가 null인 경우 split()에서 에러 발생
        - null일 때와 아닐 때 나눠서 처리
    - PlaylistController, PlaylistServiceImpl
      - modifyPlaylist 메소드에서 출력 코드 삭
    
    
    Former-commit-id: b057aee008bb27650b34e012b78c400e246b0a20

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/impl/DiaryServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/impl/DiaryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59762f1e628538df5d9b81b006fb97ea38db4bffe880f5586654cd827fbf5417
-size 15767
+oid sha256:2bd2ecabe75a7c5ac5c614793a9f960269536c9dc52db3682ee61591d939c033
+size 15888
--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/impl/PlaylistServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/impl/PlaylistServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bdfeea3e8440daa3d2ab3ba4b164beaad2186ac494274a4e5f8b9b1a8dd982c0
-size 12529
+oid sha256:0cd8c5852658ddc1f207a1aa12dcdde5ca125b8a552c4abba2c6cf6931583edd
+size 12197
