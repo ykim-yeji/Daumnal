ad2c5079b899c5ed11396beb85249afdeec9ade5
김예지 <ilice0403@naver.com>
2024-03-28 17:52:24 +0900

    fix: FastAPI Dockerfile 빌드 오류 해결을 위해 코드 수정
    Former-commit-id: b3632e0aaa7fe7580fb741245e49a84e7ccf3b32

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/constants/PlaylistConstants.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/constants/PlaylistConstants.java
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:293f97097b29b402b40175ba5d855397db2017481dffdb904868bebf4e357825
-size 209
+package com.ssafy.daumnal.music.constants;
+
+public interface PlaylistConstants {
+
+    String PLAYLIST_DEFAULT_COVER_URL = "https://daumnal.s3.ap-northeast-2.amazonaws.com/playlistCover/playlist_default.png";
+}
