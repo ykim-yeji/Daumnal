e14a3e0888155097776034308ab08564daa808b8
김예지 <ilice0403@naver.com>
2024-03-29 16:57:26 +0900

    fix: MusicServiceImpl의 addMusics 메소드에서 노래 저장 시 이미 있는 노래는 제외하고 저장하는 ê기능 추가
    Former-commit-id: e339a7809e76d67fbfcce4691a28d9d1f426049b

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/repository/MusicRepository.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/repository/MusicRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MusicRepository extends JpaRepository<Music, Long> {
+
+    boolean existsByYoutubeId(String youtubeId);
 }
