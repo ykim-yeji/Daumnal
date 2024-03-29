337165fbd4d27f2fd1266fcea9f64cf184e1284f
정성현 <gkgktjdgusdl@naver.com>
2024-03-29 16:16:36 +0900

    feat : 알림창 디자인 수정, 일별 감정 조회에서 모달 창 닫기 구현, 서비스 간판 구현, 미리보기 화면 구현
    Former-commit-id: dcdbeb2519d582a5883e357770ad479792ef515f

--- frontend/daumnal/src/components/Navigation.tsx
+++ frontend/daumnal/src/components/Navigation.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5909cad9e9fd619eec3881c472a1b540b3505650ab899c27a575c2e06912678
-size 3417
+oid sha256:fa368543d9c3202c1198cb199e5bfe9c80d198a29b87eb7c70f5861978803e0f
+size 3468
--- frontend/daumnal/src/components/modal/DiaryDetailModal.tsx
+++ frontend/daumnal/src/components/modal/DiaryDetailModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0129b7568643c4e578a9108f25ab7d6c4ac353875d4ea0ea147e426f2ae234b
-size 9240
+oid sha256:907c68069af58efeab809d51f6c9939851f1be0c0c4ff4004f8ca797f143f5ed
+size 9617
