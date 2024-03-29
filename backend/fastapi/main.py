01b26767d4d9244ccab9f31f16b3a8cc4ee9ba1e
김예지 <ilice0403@naver.com>
2024-03-29 19:15:25 +0900

    fix: fastapi Dockerfile 빌드 오류 수정
    Former-commit-id: e70edebd7a6d4b849a60b229027ed43d9cdea6bf

--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4d13fb7b17bed1f706c4cce7ebd1175593e1955bbbe3aecd08d693573c3c52c
-size 3517
+oid sha256:65dfcd900d43c79155f0f1cbab02c40d12e83c62cc213af5afbe65a5c0093cd7
+size 3516
