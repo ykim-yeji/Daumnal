54ba129bcef5042d31d80dda3e4f5faeb9242b18
jojoo <csh990720@gmail.com>
2024-03-29 17:46:54 +0900

    feat: 일기 상세 조회에서 노래 플레이리스트에 담는 기능 / 스크롤바 디자인 수정
    Former-commit-id: feecc6a210439aac08de8228346db5b97f6fd969

--- frontend/daumnal/src/components/diary/CalendarPage/DiaryMusicPlayBar.tsx
+++ frontend/daumnal/src/components/diary/CalendarPage/DiaryMusicPlayBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:839c570fb915d26e4dbb162df06ba4dc5e55896a5e15415d2e319f0adfab4336
-size 4321
+oid sha256:8c961e08bddebcdfc39ea1360b578cf3599b4eb5d366aaf907a533de3125ca2f
+size 6211
