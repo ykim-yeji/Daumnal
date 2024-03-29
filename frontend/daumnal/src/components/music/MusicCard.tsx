21939c1c738c84e0e30914525b28e046274ce317
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-03-29 13:47:09 +0900

    fix: 플레이리스트에 담기/빼기 모달 수정
    Former-commit-id: 558e2b0482748ccca1c31a4b534b56ac92b3339d

--- frontend/daumnal/src/components/music/MusicCard.tsx
+++ frontend/daumnal/src/components/music/MusicCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d03eace80cbbea6430f1f86850528c69375c2f4d1fa8ecf3b6ab2ea8f739531
-size 4357
+oid sha256:f46d075c485aa8c7bc89bd2626eb36a66e26b83fc82fcc9f862350fed7a144d2
+size 4269
