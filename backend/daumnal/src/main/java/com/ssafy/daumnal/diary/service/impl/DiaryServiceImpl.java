942d1d517a9dfb514b1ea303ef1c14b4bbeec2f9
core <jinjs6@gmail.com>
2024-03-29 11:11:29 +0900

    style: 불필요한 코드 제거
    Former-commit-id: b12cc409c117c435bc2d397fd73d47807b7abb4d

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/impl/DiaryServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/impl/DiaryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cd020fbbbf91b214462d8dd64d7af3e988da223199f84f9affedcb3ab7a5b57
-size 17461
+oid sha256:5c1f1301555bb90ce2053b3f59c829ca2bc2c51e8f70ef19e90a0276e7f4bdd0
+size 17333
