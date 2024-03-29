d2ed0386521bec64bd4e0e3232ef715d621f331f
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-03-29 13:47:09 +0900

    fix: 플레이리스트에 담기/빼기 모달 수정
    Former-commit-id: 558e2b0482748ccca1c31a4b534b56ac92b3339d

--- frontend/daumnal/src/components/modal/MusicInfoModal.tsx
+++ frontend/daumnal/src/components/modal/MusicInfoModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25b5467b17f4e50a10ebf1301396ebd91f2984ee730604ee94ee22a78f31fe6e
-size 6551
+oid sha256:15ed43b080fc6ec5595dda45c80077712a722fd6d4e964b09d86c424fed1c765
+size 6586
