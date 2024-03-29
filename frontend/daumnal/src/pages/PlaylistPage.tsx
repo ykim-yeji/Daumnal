62ed51f85a7e50b0ef104520ccde14bf7443dbd1
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-03-29 18:50:08 +0900

    feat: 노래 재생 구현중
    Former-commit-id: 21ef4a2af69dd635d011053571ad19a3f717d407

--- frontend/daumnal/src/pages/PlaylistPage.tsx
+++ frontend/daumnal/src/pages/PlaylistPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:031423ceb59bc59474bd208f1e5057b6077f749727f70ca005eee267814c2693
-size 1679
+oid sha256:a4da8ce303bce18057622a53522537abd2ff4fc43aaa80b1c61d430844a9a7f3
+size 2091
