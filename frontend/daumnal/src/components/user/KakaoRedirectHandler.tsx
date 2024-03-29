cffade996e6a4af383ac47f8b989f32cb902652d
jojoo <csh990720@gmail.com>
2024-03-29 11:17:28 +0900

    hotfix: 로그인 url 수정
    Former-commit-id: db6740ab5b1936e0c3dc7e6d8dd7ad352e12fce0

--- frontend/daumnal/src/components/user/KakaoRedirectHandler.tsx
+++ frontend/daumnal/src/components/user/KakaoRedirectHandler.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:185841879d6f7ab835094a1de7f3b1d56d3ed5d91c599f1cda5cbe306d0d29a1
+oid sha256:0ce213c53230821e79c313cd50288dfa3961e86053e40e0c6603288aef30df69
 size 5498
