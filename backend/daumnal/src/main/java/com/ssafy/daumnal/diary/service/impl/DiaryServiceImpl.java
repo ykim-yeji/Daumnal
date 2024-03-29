54ca90f2a38700805bd68b8151e0a92705979218
김예지 <ilice0403@naver.com>
2024-03-29 11:07:04 +0900

    fix: DiaryServiceImpl의 addFavoriteLyrics 메ì에서 추가할 가사 문장 번호 배열  오름차순으로 정렬 후 저장í하도록 수정
    Former-commit-id: f8d24f3e717bb0c883840149c96ebf8cfbeb8249

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/impl/DiaryServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/impl/DiaryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4faba39e2c80c0cb8f56012273135899ea57804781be098b9b151d02e390bd81
-size 17782
+oid sha256:48504b40644ed9e79bfd8efc65d3ea5001458f84e942aba3e59d865722cf308c
+size 17875
