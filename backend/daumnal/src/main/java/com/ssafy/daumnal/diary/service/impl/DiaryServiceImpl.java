f5edde92bc8ff6ea6de34e704410711d50a6491f
김예지 <ilice0403@naver.com>
2024-03-29 10:58:33 +0900

    fix: DiaryServiceImpl의 GetLyricsOfTodayRecommendedMusic에서 Diary의 lyricsLineNumber이 null 또는 비어있을 때는 빈 배열로 응답하도록 함
    Former-commit-id: 18b44d8f6eab2e38903951ee0afd9c20645c4cb4

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/impl/DiaryServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/impl/DiaryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:008baaa2db199520daad513f561921b5970c809094aab39f8d7b9c085ccea74e
-size 17740
+oid sha256:4faba39e2c80c0cb8f56012273135899ea57804781be098b9b151d02e390bd81
+size 17782
