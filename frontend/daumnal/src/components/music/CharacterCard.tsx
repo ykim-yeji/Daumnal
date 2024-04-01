f14fa38abbac0d3a6f3ad893972b1afcbbce4f81
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-04-01 17:32:56 +0900

    feat: 캐릭터 선택 페이지 키워드 추가
    Former-commit-id: 3b0ee04ab284b96a8442e0851e7ca3cedbf87784

--- frontend/daumnal/src/components/music/CharacterCard.tsx
+++ frontend/daumnal/src/components/music/CharacterCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:986f1333d4c22ed3fc634e705ac9b219c3fc855824c6efcd710480298255c3bc
-size 2478
+oid sha256:478d002b68cf76cf4959e61517d5ffd4ac30f6828aba09d35603fc36b513a692
+size 2578
