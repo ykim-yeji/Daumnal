2aaa856df0272e22c29c675b6ac6cee126e96b35
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-04-01 10:57:00 +0900

    feat: 플레이리스 내부 노래 재생 관련 동작 구현 완료
    Former-commit-id: fd16ec81a834b58463b99188845e9fa2d1103fb4

--- frontend/daumnal/src/components/modal/LyricsModal.tsx
+++ frontend/daumnal/src/components/modal/LyricsModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce851cb3f31509295918a4d43c6d845d854193edb38c87dd65b63e317e9720e3
-size 1329
+oid sha256:682c0ef7fa50c9a2fded989d9ba11b935bf2289ded11861f295b5a950a6a367f
+size 1503
