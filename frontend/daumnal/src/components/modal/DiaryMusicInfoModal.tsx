cc4193706e263a3bb99a29f909388ae36894e1a2
jojoo <csh990720@gmail.com>
2024-04-01 17:46:25 +0900

    hot fix: 노래 삭제 알림창 추가
    Former-commit-id: 2b286007455a42fb45ca6415d0dbeac6869edffe

--- frontend/daumnal/src/components/modal/DiaryMusicInfoModal.tsx
+++ frontend/daumnal/src/components/modal/DiaryMusicInfoModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a55d38506eddc9a29cec218265f5858c138d087b38bd403d7cd12ce85e112bf4
-size 6735
+oid sha256:26f8d51bfd2f5e38506b2408e2a7db7683b895f519e3022530be7f43bb3ed0f5
+size 6862
