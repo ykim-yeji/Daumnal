bcb9d2a96578a170f0b049d8f94caf368800ee5c
jojoo <csh990720@gmail.com>
2024-04-01 17:42:37 +0900

    fix: 알림창 추가
    Former-commit-id: e231f67e83b7aa0c6615c49f2f0f426d057c9dd0

--- frontend/daumnal/src/components/modal/DiaryMusicInfoModal.tsx
+++ frontend/daumnal/src/components/modal/DiaryMusicInfoModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79b1156355fdada2d3865adfe16d5afc989ecfc9d26c18cd23d2c0daf66084f3
-size 6586
+oid sha256:a55d38506eddc9a29cec218265f5858c138d087b38bd403d7cd12ce85e112bf4
+size 6735
