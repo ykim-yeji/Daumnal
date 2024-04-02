e63a4daf8c11f4a1ec2af6d48f042d497378c6a9
전새벽 <wjswwkd@naver.com>
2024-04-02 17:33:51 +0900

    fix: 중복 추천 예외처리 수정
    Former-commit-id: 6f02a216bcab4e79aab0ca4620ae462e58bdae04

--- backend/fastapi/recommend.py
+++ backend/fastapi/recommend.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4232527150ff1bb57f2457da721a1b250b45b5ff3140d35c7840d9fedbd81360
-size 1484
+oid sha256:38c46792510def6b00773e281773af9e8d01587fd56a63126dd96bdb646632fd
+size 1559
