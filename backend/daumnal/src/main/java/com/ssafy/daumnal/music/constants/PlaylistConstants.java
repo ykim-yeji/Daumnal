f5dc89fcd7b17c589fbdd104cb42b6286264c97b
김예지 <ilice0403@naver.com>
2024-04-02 10:45:09 +0900

    fix: Music, Playlist coverUrl 컬럼에서 디폴트 값 불가능 오류 해결
    - S3Path
      - MUSIC_COVER_PATH 추가
      - PLAYLIST_COVER_PATH 이름 변경
    - Playlist, Music
      - coverUrl 데이터 타입을 TEXT -> Varchar(3000)으로 변경
    - MusicConstants
      - MUSIC_DEFAULT_COVER_URL 추가
    
    
    Former-commit-id: 94f3e127eb78488c5770015ca580163dc99b3ce0

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/constants/PlaylistConstants.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/constants/PlaylistConstants.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:293f97097b29b402b40175ba5d855397db2017481dffdb904868bebf4e357825
-size 209
+oid sha256:d668216bb900cf2a22a8c8a02b43beb27797bb4e23e9db23aae6874abeb5a612
+size 217
