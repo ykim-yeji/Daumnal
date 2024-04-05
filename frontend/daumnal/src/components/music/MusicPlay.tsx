77005d294d11e97b9046a2e500b6430114b2d9ee
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-04-05 18:50:10 +0900

    fix: 볼륨 조절
    Former-commit-id: 98c28fe8c1dce2f364011b5b0f18d74393968594

--- frontend/daumnal/src/components/music/MusicPlay.tsx
+++ frontend/daumnal/src/components/music/MusicPlay.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66e79770f09eec3e6f2bc310f09d9c23afef989400727b1b2443dbb19fe76c3f
-size 11548
+oid sha256:2c633914befb02e3958ba6c88f037d202805a87eddcb253741d2e8914c74c4f1
+size 11547
