bf24775fe79827cca3f2b3040170cffc59bd3a76
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-04-05 18:36:02 +0900

    fix: 플레이리스트 전체화면 비율에 맞게 수정 및 볼륨 조절
    Former-commit-id: 17d9c537ec882fb75ebb544170ae35a89af746cb

--- frontend/daumnal/src/components/modal/EditPlaylistModal.tsx
+++ frontend/daumnal/src/components/modal/EditPlaylistModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa1a78b6ee2cbfbfa38d6a4119184a72f167b0fc83cd72a2934045f23959d745
-size 7604
+oid sha256:f5368dff7d34ccef8994b1b1294e84ba4238b574f8590610530e938f6a85a733
+size 7602
