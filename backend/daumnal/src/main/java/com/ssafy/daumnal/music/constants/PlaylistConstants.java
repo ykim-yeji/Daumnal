f4fb93d777fa30ae2c7a71292e744b4a03a2edc6
ykim-yeji <68416691+ykim-yeji@users.noreply.github.com>
2024-04-11 20:11:13 +0900

    fix: playlist cover url constants 변경
    Former-commit-id: 7396f44b2282893b56ceb2418bbc07d6dc771f31
    Former-commit-id: b1f67527dbbb5ed3482398cbbdeb61417f8610f7

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/constants/PlaylistConstants.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/constants/PlaylistConstants.java
@@ -2,7 +2,7 @@ package com.ssafy.daumnal.music.constants;
 
 public interface PlaylistConstants {
 
-    String PLAYLIST_DEFAULT_COVER_URL_DB = "'https://daumnal.s3.ap-northeast-2.amazonaws.com/PLAYLIST_COVER_PATH/playlist_default.png'";
+    String PLAYLIST_DEFAULT_COVER_URL_DB = "'https://daumnal.s3.ap-northeast-2.amazonaws.com/PRODUCTION_SERVER/PLAYLIST_COVER_PATH/playlist_default.png'";
 
-    String PLAYLIST_DEFAULT_COVER_URL = "https://daumnal.s3.ap-northeast-2.amazonaws.com/PLAYLIST_COVER_PATH/playlist_default.png";
+    String PLAYLIST_DEFAULT_COVER_URL = "https://daumnal.s3.ap-northeast-2.amazonaws.com/PRODUCTION_SERVER/PLAYLIST_COVER_PATH/playlist_default.png";
 }
