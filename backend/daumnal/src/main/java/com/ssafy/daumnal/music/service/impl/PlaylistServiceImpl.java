d0a1d445eeda744379aaed99f1bdfbfa36b7b6b6
ykim-yeji <68416691+ykim-yeji@users.noreply.github.com>
2024-04-11 19:43:04 +0900

    fix: 플레이리스트 기본 커버 s3 삭제 오류 수정
    Former-commit-id: 34eccf5d6536371ba38ce133a70295beaa5102f4

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/impl/PlaylistServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/impl/PlaylistServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d46a1d66f73aecdc6fb277a07995454bd9b34ae1929e9f784426c99efc341c6
-size 12265
+oid sha256:2e833aba1eba986e8ce1bb017dd6aa8452dd3b93e0d50990c038e2e48a44f6f0
+size 12353
